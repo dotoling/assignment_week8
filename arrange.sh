212c2c01dda14fb54a2ee861361ddf8f1fbaf7e2
dotoling <hegnut5859@gmail.com>
2023-04-20 19:13:50 +0900

    [Add] arrange script file

---
+++ arrange.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+cd files
+
+for fileName in A*; do mv "$fileName" "../a" ; done
+for fileName in a*; do mv "$fileName" "../a" ; done
+for fileName in B*; do mv "$fileName" "../b" ; done
+for fileName in b*; do mv "$fileName" "../b" ; done
+for fileName in C*; do mv "$fileName" "../c" ; done
+for fileName in c*; do mv "$fileName" "../c" ; done
+for fileName in D*; do mv "$fileName" "../d" ; done
+for fileName in d*; do mv "$fileName" "../d" ; done
+for fileName in E*; do mv "$fileName" "../e" ; done
+for fileName in e*; do mv "$fileName" "../e" ; done
+for fileName in F*; do mv "$fileName" "../f" ; done
+for fileName in f*; do mv "$fileName" "../f" ; done
+for fileName in G*; do mv "$fileName" "../g" ; done
+for fileName in g*; do mv "$fileName" "../g" ; done
+for fileName in H*; do mv "$fileName" "../h" ; done
+for fileName in h*; do mv "$fileName" "../h" ; done
+for fileName in I*; do mv "$fileName" "../i" ; done
+for fileName in i*; do mv "$fileName" "../i" ; done
+for fileName in J*; do mv "$fileName" "../j" ; done
+for fileName in j*; do mv "$fileName" "../j" ; done
+for fileName in K*; do mv "$fileName" "../k" ; done
+for fileName in k*; do mv "$fileName" "../k" ; done
+for fileName in L*; do mv "$fileName" "../l" ; done
+for fileName in l*; do mv "$fileName" "../l" ; done
+for fileName in M*; do mv "$fileName" "../m" ; done
+for fileName in m*; do mv "$fileName" "../m" ; done
+for fileName in N*; do mv "$fileName" "../n" ; done
+for fileName in n*; do mv "$fileName" "../n" ; done
+for fileName in O*; do mv "$fileName" "../o" ; done
+for fileName in o*; do mv "$fileName" "../o" ; done
+for fileName in P*; do mv "$fileName" "../p" ; done
+for fileName in p*; do mv "$fileName" "../p" ; done
+for fileName in Q*; do mv "$fileName" "../q" ; done
+for fileName in q*; do mv "$fileName" "../q" ; done
+for fileName in R*; do mv "$fileName" "../r" ; done
+for fileName in r*; do mv "$fileName" "../r" ; done
+for fileName in S*; do mv "$fileName" "../s" ; done
+for fileName in s*; do mv "$fileName" "../s" ; done
+for fileName in T*; do mv "$fileName" "../t" ; done
+for fileName in t*; do mv "$fileName" "../t" ; done
+for fileName in U*; do mv "$fileName" "../u" ; done
+for fileName in u*; do mv "$fileName" "../u" ; done
+for fileName in V*; do mv "$fileName" "../v" ; done
+for fileName in v*; do mv "$fileName" "../v" ; done
+for fileName in W*; do mv "$fileName" "../w" ; done
+for fileName in w*; do mv "$fileName" "../w" ; done
+for fileName in X*; do mv "$fileName" "../x" ; done
+for fileName in x*; do mv "$fileName" "../x" ; done
+for fileName in Y*; do mv "$fileName" "../y" ; done
+for fileName in y*; do mv "$fileName" "../y" ; done
+for fileName in Z*; do mv "$fileName" "../z" ; done
+for fileName in z*; do mv "$fileName" "../z" ; done
